9f9784f20ca3183f82b4e6aacfae2d0f2e1cf8ab
davidHigginbotham <davidrayhigginbotham@gmail.com>
2020-04-26 11:51:05 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,20 +16,19 @@ export class ProductListComponent {
  
   checkout() {
 
-    cart = [];
-
     for (let i = 0; i < products.length; i++) {
 
       if(products[i].quantity > 0) {
           console.log(products[i].name);
-          this.cart.push(product);
+          this.cart.push(products[i]);
       }
-
 
     }
 
-    if(cart.lngth > 0) {
+    if(this.cart.length > 0) {
+
       window.alert("Checkout submitted");
+
     }
   }
 
