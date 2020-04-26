aeed3faf7abf61ac83555419716c9d7c8d35cca8
davidHigginbotham <davidrayhigginbotham@gmail.com>
2020-04-26 09:38:37 -0500

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,41 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    prodmods : [
+    {
+      name :"prod mod 1"
+    },
+    {
+      name : "prod mod 2"
+    }
+    ]
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    prodmods : [
+    {
+      name :"prod mod 3"
+    },
+    {
+      name : "prod mod 4"
+    }
+    ]
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    prodmods : [
+    {
+      name :"prod mod 5"
+    },
+    {
+      name : "prod mod 6"
+    }
+    ]
   }
 ];
 
