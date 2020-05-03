01a2ea4cc13b69b907a3bd92311b5c71b9da097e
davidHigginbotham <davidrayhigginbotham@gmail.com>
2020-05-03 08:18:30 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';	
+import { Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
@@ -12,14 +14,38 @@ export class ProductDetailsComponent implements OnInit {
 
    product;
 
+  @Output() notify = new EventEmitter();	
+  isOptionsToggle = false;	
+
   constructor(
     private route: ActivatedRoute,
   ) { }
 
  ngOnInit() {
+   this.isOptionsToggle = false;	
   this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
 }
+
+  toggleOptions() {	
+    this.isOptionsToggle = !this.isOptionsToggle;	
+  }	
+
+  addQuantity () {	
+    this.product.quantity = this.product.quantity + 1;	
+    window.alert("added");	
+     window.alert(this.product.quantity);	
+  };	
+
+  subtractQuantity () {	
+    window.alert("subtractQuantity");	
+    window.alert(this.product.quantity);	
+    if(this.product.quantity.length > 0) {	
+      window.alert("substracted");	
+      window.alert(this.product.quantity);	
+        this.product.quantity = this.product.quantity - 1;	
+    }	
+  };
 
 }
