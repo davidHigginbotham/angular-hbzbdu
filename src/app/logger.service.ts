19bb3bec1e88c0640915c9041689e17ce6727e68
davidHigginbotham <davidrayhigginbotham@gmail.com>
2020-05-03 08:20:11 -0500

    5 files modified

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable()
-export class LoggerService {
+// @Injectable()
+// export class LoggerService {
 
-  constructor() { }
+//   constructor() { }
 
-  log(msg: any)   { console.log(msg); }
-  error(msg: any) { console.error(msg); }
-  warn(msg: any)  { console.warn(msg); }
+//   log(msg: any)   { console.log(msg); }
+//   error(msg: any) { console.error(msg); }
+//   warn(msg: any)  { console.warn(msg); }
 
-}
+// }
--- src/app/menu-service.ts
+++ src/app/menu-service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable, throwError } from 'rxjs';
+// import { catchError, retry } from 'rxjs/operators';
 
-@Injectable()
-export class MenuService {
-  private products: any[] = [];
+// @Injectable()
+// export class MenuService {
+//   private products: any[] = [];
 
-  constructor(
-    private http: HttpClient,
-    private logger: Logger) { }
+//   constructor(
+//     private http: HttpClient,
+//     private logger: Logger) { }
 
-  getProducts() {
-    this.http.get..getAll(Product).then( (products: Product[]) => {
-      this.logger.log(`Fetched ${products.length} products.`);
-      this.products.push(...products); // fill cache
-    });
-    return this.products;
-  }
-}
+//   getProducts() {
+//     this.http.get..getAll(Product).then( (products: Product[]) => {
+//       this.logger.log(`Fetched ${products.length} products.`);
+//       this.products.push(...products); // fill cache
+//     });
+//     return this.products;
+//   }
+// }
